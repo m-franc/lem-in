91d59375660487849b29e082b265a24c31fb9341
Maxime FRANC <mfranc@e2r5p17.42.fr>
2017-06-28 12:38:40 +0200

    init function

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/27 16:32:54 by mfranc            #+#    #+#             */
-/*   Updated: 2017/06/27 17:00:27 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/06/28 12:19:14 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,41 @@
 # include "libft.h"
 # include "get_next_line.h"
 # include "ft_printf.h"
+
+typedef struct			s_data_rooms
+{
+	char				*name;
+	int					tunnels;
+	int					start;
+	int					end;
+	int					x;
+	int					y;
+	struct s_data_rooms	*next;
+	struct s_data_rooms	*prev;
+}						t_data_rooms;
+
+typedef struct			s_ctl_data_rooms
+{
+	int					size;
+	t_data_rooms		*first;
+	t_data_rooms		*last;
+}						t_ctl_data_rooms;
+
+typedef struct			s_tunnels
+{
+	char				*from;
+	char				*to;
+	struct s_tunnels	*next;
+}						t_tunnels;
+
+typedef struct			s_data_store
+{
+	int					nb_ants;
+	t_ctl_data_rooms	*rooms;
+	t_tunnels			*tunnels;
+}						t_data_store;
+
+t_data_store			*ft_data_store_init(void);
+t_ctl_data_rooms		*ft_data_rooms_init(void);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,42 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/27 16:11:58 by mfranc            #+#    #+#             */
-/*   Updated: 2017/06/27 16:30:21 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/06/28 12:22:18 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
 
-int main(int ac, char **av)
+t_ctl_data_rooms		*ft_data_rooms_init(void)
 {
+	t_ctl_data_rooms	*rooms;
+
+	if (!(rooms = (t_ctl_data_rooms*)malloc(sizeof(t_ctl_data_rooms))))
+		exit(-1);
+	rooms->size = 0;
+	rooms->first = NULL;
+	rooms->last = NULL;
+	return (rooms);
+}
+
+t_data_store			*ft_data_store_init(void)
+{
+	t_data_store 		*data_store;
+
+	if (!(data_store = (t_data_store*)malloc(sizeof(t_data_store))))
+		exit(-1);
+	data_store->nb_ants = 0;
+	data_store->rooms = ft_data_rooms_init();
+	data_store->tunnels = NULL;
+	return (data_store);
+}
+
+int 					main(int ac, char **av)
+{
+	t_data_store		*data_store;
+
 	(void)ac;
 	(void)av;
-	ft_putendl("ok");
+	data_store = ft_data_store_init();
+	return (0);
 }
