a9abb29a705aacddcb971594c9090dc322b247bc
Maxime FRANC <mfranc@e2r7p19.42.fr>
2017-06-29 18:44:17 +0200

    store room in progress

--- srcs/parsing/ft_store_command.c
+++ srcs/parsing/ft_store_command.c
@@ -6,7 +6,7 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/29 11:27:49 by mfranc            #+#    #+#             */
-/*   Updated: 2017/06/29 17:48:11 by mfranc           ###   ########.fr       */
+/*   Updated: 2017/06/29 18:39:25 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,48 @@ int				ft_store_ants_number(t_data_store *data_store, char *ants_number)
 	return (1);
 }
 
-//int				ft_store_room(t_data_store *data_store, char **data_room)
-//{
-//	if (data_store->rooms)
-//		ft_check_double()
-//	return (1);
-//}
+static int		ft_check_duplicate(t_data_rooms *last_rooms, char **data_room)
+{
+	while (last_rooms)
+	{
+		if (ft_strcmp(last_rooms->name, data_room[0]) == 0
+				&& last_rooms->x == ft_atoi(data_room[1])
+				&& last_rooms->y == ft_atoi(data_room[2]))
+			return (0);
+		else if (ft_strcmp(last_rooms->name, data_room[0]) != 0
+				&& last_rooms->x == ft_atoi(data_room[1])
+				&& last_rooms->y == ft_atoi(data_room[2]))
+			return (0);
+		last_rooms = last_rooms->next;
+	}
+	return (1);
+}
+
+int					ft_store_room(t_data_store *data_store, char **data_room)
+{
+	t_data_rooms	*last_rooms;
+	t_data_rooms	*new_room;
+
+	last_rooms = data_store->rooms;
+	if (last_rooms)
+		if (!ft_check_duplicate(last_rooms, data_room))	
+			return (0);	
+	last_rooms = data_store->rooms;
+	if (!(new_tunnel = (t_data_rooms*)malloc(sizeof(t_data_rooms))))
+		exit(-1);
+	new_room->name = data_room[0];
+	new_room->tunnels = 0;
+	new_room->start = 0;
+	new_room->end = 0;
+	if (data_store->start_mark == 1 && data_store->start_mark == 1)
+		new_room->start = 1;
+	if (data_store->end_mark == 1 && data_store->end_mark == 0)
+		new_room->end = 1;
+	new_room->x = ft_atoi(tab[1]);
+	new_room->y = ft_atoi(tab[2]);
+	if (!last_rooms)
+		data_store->rooms = new_room;
+	else
+		ft_push_back_room(&last_rooms, new_room);
+	return (1);
+}
